ef9b81d3ad281376f90375bb6a9fc92e4837ebd7
rohitnaruka <narukarohitsingh8@gmail.com>
2025-12-16 14:21:15 +0530

    NORMAL CALCULATOR

---
+++ project 1.py
@@ -0,0 +1,22 @@
+A=float(input("enter the first number-:"))
+B=float(input("enter the second number-:"))
+
+print("Choose operation:")
+
+
+choice = input("Enter operation: ")
+
+# logic
+if choice == "+":
+    print("Result:", A + B)
+
+elif choice == "-":
+    print("Result:", A - B)
+
+elif choice == "*":
+    print("Result:", A * B)
+
+elif choice == "/":
+    # if num2 != 0:
+        print("Result:", A / B)
+
